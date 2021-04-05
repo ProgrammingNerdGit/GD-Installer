d3bd5163e3e86479bf423a2f631366c0c15b21b0
Programming Nerd <42655900+ProgrammingNerdGit@users.noreply.github.com>
2021-04-05 10:07:20 -0400

    Added linux support

--- zipper.h
+++ zipper.h
@@ -4,23 +4,45 @@
 #include <fstream>
 #include "String/String.h"
 
+#ifdef __unix__
+    #define OS_WIN 0
+#elif
+    #define OS_WIN 1
+#endif
 using vs = std::vector<std::string>;
 
 bool zip(vs files){
     bool flag(false);
     system("mkdir tmp");
-    std::string com("@echo off && copy /Y \"{f}\" tmp");
-    for(std::string file: files){
-        std::ifstream tst(file);
-        if(!tst.is_open()){return 0;}
-        system(spp::replace(com,"{f}",file).c_str());
-        tst.close();
+   
+
+    if(OS_WIN){
+        std::string com("@echo off && copy /Y \"{f}\" tmp");
+        for(std::string file: files){
+            std::ifstream tst(file);
+            if(!tst.is_open()){return 0;}
+            system(spp::replace(com,"{f}",file).c_str());
+            tst.close();
+        }
+        system("zip.vbs");
+        system("del /F /Q tmp");
+    }else{
+        system("sudo apt-get install zip");
+        std::string com("cp \"{f}\" tmp");
+        for(std::string file: files){
+            std::ifstream tst(file);
+            if(!tst.is_open()){return 0;}
+            system(spp::replace(com,"{f}",file).c_str());
+            std::cout << spp::replace(com,"{f}",file) << "\n";
+            tst.close();
+        }
+        system("zip tmp.zip tmp/*");
+        system("rm -f tmp/*");
     }
-
-    system("zip.vbs");
-
-    system("del /F /Q tmp");
+    
     system("rmdir tmp");
+
+    
 
     return 1;
     
